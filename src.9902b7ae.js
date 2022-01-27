6436fed5bb307e5b2ea731b635d4acd184438713
DenysTutov <93935076+DenysTutov@users.noreply.github.com>
2022-01-27 18:27:52 +0000

    Deploying to gh-pages from @ DenysTutov/goit-markup-project-group14@5b4b5a8d6262aea63099520248fc307505e611aa 🚀

--- src.9902b7ae.js
+++ src.9902b7ae.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-markup-project-group14/src.d49ece40.js.map
+//# sourceMappingURL=/goit-markup-project-group14/src.9902b7ae.js.map
