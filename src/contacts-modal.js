39e1a5445675ecedc1a1fb24ae20757e4517e760
DenysTutov <93935076+DenysTutov@users.noreply.github.com>
2022-02-02 14:23:03 +0200

    add bagfix modal

--- src/contacts-modal.js
+++ src/contacts-modal.js
@@ -8,7 +8,8 @@
     refs.openContactModalBtn.addEventListener('click', toggleModal);
     refs.closeContactModalBtn.addEventListener('click', toggleModal);
 
-    function toggleModal() {
+  function toggleModal() {
+      document.body.classList.toggle('modal-open');
       refs.contactModal.classList.toggle('is-hidden');
     }
-  })();
+})();
